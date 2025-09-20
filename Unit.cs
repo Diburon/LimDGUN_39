bde2caab80bc835c146202070f41a4398f619b86
Диана <lim.budennovsky@gmail.com>
2025-09-20 14:38:12 +0300

    Add files via upload

---
+++ Unit.cs
@@ -0,0 +1,26 @@
+﻿public class Unit
+{
+    public string Name{get;}
+    public float Health=>@health;
+    public int Damage{get;}
+    public float Armor{get;}
+    private float @health;
+
+    public Unit():this("Unknown Unit"){
+    }
+    public Unit(string name)
+    {
+        Name = name;
+        @health = 100f;
+        Damage = 5;
+        Armor = 0.6f;
+    }
+    public float GetRealHealth()
+    {
+        return @health * (1f + Armor);
+    }
+    public bool SetDamage(float damage){
+        @health -= damage * (1f - Armor);
+        return @health <= 0f;
+    }
+}
